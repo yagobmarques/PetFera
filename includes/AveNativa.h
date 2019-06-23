56fe261b80d84242905ffedbda3303d3dbc9273a
João Vítor Fonseca de Mendonça <joovitor177@gmail.com>
2019-06-23 18:12:12 -0300

    Esqueleto

---
+++ includes/AveNativa.h
@@ -0,0 +1,14 @@
+#ifndef _AVE_NATIVA_
+#define _AVE_NATIVA_
+using namespace std;
+#include "../Ave.h"
+class AveNativa : public Ave{
+    private:
+ 
+        
+    public:          
+        AveNativa();
+        ~AveNativa();
+        //sobrecarga de operadores caso presice aqui em baixo     
+};
+#endif
