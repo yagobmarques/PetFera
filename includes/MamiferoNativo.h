70dbd0006808155cfd5385651778c670da26614d
João Vítor Fonseca de Mendonça <joovitor177@gmail.com>
2019-06-23 18:14:29 -0300

    Esqueleto

---
+++ includes/MamiferoNativo.h
@@ -0,0 +1,15 @@
+#ifndef _MAMIFERO_NATIVO_
+#define _MAMIFERO_NATIVO_
+
+#include "./Mamifero.h"
+
+class MamiferoNativo : public Mamifero{
+    private:
+    
+        
+    public:          
+        MamiferoNativo();
+        ~MamiferoNativo();
+        //sobrecarga de operadores caso presice aqui em baixo:     
+};
+#endif
