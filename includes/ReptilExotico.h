216afaaab26653ce5b33104bd44a9e3ea1ec1766
João Vítor Fonseca de Mendonça <joovitor177@gmail.com>
2019-06-23 18:15:23 -0300

    Esqueleto

---
+++ includes/ReptilExotico.h
@@ -0,0 +1,14 @@
+#ifndef _REPTIL_EXOTICO_
+#define _REPTIL_EXOTICO_
+#include "./Reptil.h"
+class ReptilExotico : public Reptil{
+    private:
+        bool m_venenoso;
+        string m_tipo_veneno;
+        
+    public:          
+        ReptilExotico();
+        ~ReptilExotico();
+        //sobrecarga de operadores:     
+};
+#endif
