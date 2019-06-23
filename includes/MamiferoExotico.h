891673e09cb3e01d32056dc97dc2eeda62135abd
João Vítor Fonseca de Mendonça <joovitor177@gmail.com>
2019-06-23 18:13:41 -0300

    Esqueleto

---
+++ includes/MamiferoExotico.h
@@ -0,0 +1,14 @@
+#ifndef _MAMIFERO_EXOTICO_
+#define _MAMIFERO_EXOTICO_
+using namespace stf;
+#include "./Mamifero.h"
+class MamiferoExotico : public Mamifero{
+    private:
+    
+        
+    public:          
+        MamiferoExotico();
+        ~MamiferoExotico();
+        //sobrecarga de operadores caso presice aqui em baixo     
+};
+#endif
