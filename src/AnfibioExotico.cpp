0f1c325da8e69a3976ef89e180fba9407ba26def
João Vítor Fonseca de Mendonça <joovitor177@gmail.com>
2019-06-23 17:48:30 -0300

    Update AnfibioExotico.cpp

--- src/AnfibioExotico.cpp
+++ src/AnfibioExotico.cpp
@@ -1,1 +1,12 @@
-#include "./AnfibioExotico.h"
+#ifndef _ANFIBIOEXOTICO_
+#define _ANFIBIOEXOTICO_
+#include "./Anfibio.h"
+class AnfibioExotico : public Anfibio{
+    private:
+
+    public:
+     AnfibioExotico();
+    ~AnfibioExotico();
+    //sobrecarga de operadores caso precise aqui em baixo
+};
+#endif
